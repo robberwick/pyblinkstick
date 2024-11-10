2a3c306a0e2eae9c9a2d9dd9e4d1ba5e39c36968
Rob Berwick <rob.berwick@gmail.com>
2024-11-10 09:21:58 +0000

    refactor: update test fixtures
    update the get_color tests to use the blinkstick fixture

--- tests/clients/test_blinkstick.py
+++ tests/clients/test_blinkstick.py
@@ -65,25 +65,25 @@ def test_get_variant_string(blinkstick, expected_variant, expected_name):
     assert blinkstick.get_variant_string() == expected_name
 
 
-def test_get_color_rgb_color_format(mocker: MockFixture):
+def test_get_color_rgb_color_format(mocker: MockFixture, blinkstick: BlinkStick):
     """Test get_color with color_format='rgb'. We expect it to return the color in RGB format."""
-    mock_get_color_rgb = mocker.patch.object(BlinkStick, '_get_color_rgb', return_value=(255, 0, 0))
-    blinkstick = BlinkStick()
+    mock_get_color_rgb = mocker.Mock(return_value=(255, 0, 0))
+    blinkstick._get_color_rgb = mock_get_color_rgb
     assert blinkstick.get_color() == (255, 0, 0)
     assert mock_get_color_rgb.call_count == 1
 
 
-def test_get_color_hex_color_format(mocker):
+def test_get_color_hex_color_format(mocker: MockFixture, blinkstick: BlinkStick):
     """Test get_color with color_format='hex'. We expect it to return the color in hex format."""
-    mock_get_color_hex = mocker.patch.object(BlinkStick, '_get_color_hex', return_value='#ff0000')
-    blinkstick = BlinkStick()
+    mock_get_color_hex = mocker.Mock(return_value='#ff0000')
+    blinkstick._get_color_hex = mock_get_color_hex
     assert blinkstick.get_color(color_format='hex') == '#ff0000'
     assert mock_get_color_hex.call_count == 1
 
 
-def test_get_color_invalid_color_format(mocker):
+def test_get_color_invalid_color_format(mocker: MockFixture, blinkstick: BlinkStick):
     """Test get_color with invalid color_format. We expect it not to raise an exception, but to default to RGB."""
-    mock_get_color_rgb = mocker.patch.object(BlinkStick, '_get_color_rgb', return_value=(255, 0, 0))
-    blinkstick = BlinkStick()
+    mock_get_color_rgb = mocker.Mock(return_value=(255, 0, 0))
+    blinkstick._get_color_rgb = mock_get_color_rgb
     blinkstick.get_color(color_format='invalid_format')
     assert mock_get_color_rgb.call_count == 1
