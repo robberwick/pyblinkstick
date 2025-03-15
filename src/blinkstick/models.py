588fc448fc4cac0be014fee4cbe67d6eb40060c6
Rob Berwick <rob.berwick@gmail.com>
2025-03-15 12:53:34 +0000

    feat: add Color dataclass for RGB color representation with validation

--- src/blinkstick/models.py
+++ src/blinkstick/models.py
@@ -30,3 +30,30 @@ class SerialDetails:
         object.__setattr__(self, "sequence_number", int(match.group(1)))
         object.__setattr__(self, "major_version", int(match.group(2)))
         object.__setattr__(self, "minor_version", int(match.group(3)))
+
+
+@dataclass()
+class Color:
+    """
+    A color representation.
+
+    :param red: Red component of the color (0-255)
+    :param green: Green component of the color (0-255)
+    :param blue: Blue component of the color (0-255)
+    """
+
+    red: int = 0
+    green: int = 0
+    blue: int = 0
+
+    def __post_init__(self):
+        if not all(0 <= value <= 255 for value in (self.red, self.green, self.blue)):
+            raise ValueError("Color values must be between 0 and 255")
+
+    def __iter__(self):
+        """
+        Iterate over the color components.
+        """
+        yield self.red
+        yield self.green
+        yield self.blue
